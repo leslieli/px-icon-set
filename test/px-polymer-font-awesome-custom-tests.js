ce51886b95e2984af6edd015edfc7d59bc8d0adb
Runn Vermel <runn.vermel@ge.com>
2016-11-22 10:11:29 -0800

    did a major bump, updated license, fixed README, removed dummy tests and replaced with our test framework, renamed file to match folder and component name, added wct config file.

---
+++ test/px-polymer-font-awesome-custom-tests.js
@@ -0,0 +1,9 @@
+// This is the wrapper for custom tests, called upon web components ready state
+function runCustomTests() {
+  // This is the placeholder suite to place custom tests in
+  // Use testCase(options) for a more convenient setup of the test cases
+
+  suite('Custom Automation Tests for px-polymer-font-awesome', function() {
+
+  });
+}
