6ffaf093e07b7ad96639b0bc8fcfbf1af1992bb9
Elliot Nahman <elliot.nahman@ge.com>
2017-07-17 13:48:42 -0700

    fix test

--- test/px-icon-set-custom-tests.js
+++ test/px-icon-set-custom-tests.js
@@ -1,11 +1,10 @@
 // This is the wrapper for custom tests, called upon web components ready state
 function runCustomTests() {
-  // This is the placeholder suite to place custom tests in
-  // Use testCase(options) for a more convenient setup of the test cases
-
   suite('Custom Automation Tests for px-icon', function() {
-    var icon = document.getElementById('px_icon'),
-        iron = icon.querySelector('iron-icon');
-    assert.equal(iron.icon, 'px:aircraft');
+    test('icon', function() {
+      var icon = document.getElementById('px_icon'),
+          iron = icon.querySelector('iron-icon');
+      assert.equal(iron.icon, 'px:aircraft');
+    });
   });
 }
