1ccaa96a9917a36e2f8125b6cade2738bd5c02ba
Elliot Nahman <elliot.nahman@ge.com>
2017-07-27 10:27:10 -0700

    update tests

--- test/px-icon-set-custom-tests.js
+++ test/px-icon-set-custom-tests.js
@@ -6,5 +6,23 @@ function runCustomTests() {
           iron = icon.querySelector('iron-icon');
       assert.equal(iron.icon, 'px-fea:bug');
     });
+    test('sizes', function() {
+      var icon = document.getElementById('px_icon'),
+          iron = icon.querySelector('iron-icon');
+      assert.equal(iron.getComputedStyleValue('--iron-icon-width'), '32px');
+      assert.equal(iron.getComputedStyleValue('--iron-icon-height'), '32px');
+    });
+    test('updates icon', function() {
+      var icon = document.getElementById('px_icon'),
+          iron = icon.querySelector('iron-icon');
+      icon.icon = 'px-nav:favorite';
+      assert.equal(iron.icon, 'px-nav:favorite');
+    });
+    test('updates size', function() {
+      var icon = document.getElementById('px_icon'),
+          iron = icon.querySelector('iron-icon');
+      assert.equal(iron.getComputedStyleValue('--iron-icon-width'), '22px');
+      assert.equal(iron.getComputedStyleValue('--iron-icon-height'), '22px');
+    });
   });
 }
